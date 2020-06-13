a24c4d6cfcee4bfa607bc454b4e315415b45812b
luucamay <lupemaydana@gmail.com>
2020-06-13 16:33:51 -0400

    feat: Create test

---
+++ test/pages/SignIn.spec.js
@@ -0,0 +1,11 @@
+import SignIn from '../../src/pages/SignIn';
+describe('SignIn', () => {
+  it('should return div with nested button', () => {
+    const el = SignIn();
+    expect(el.tagName).toBe('DIV');
+    expect(el.className).toBe('sign-in');
+    expect(el.children.length).toBe(1);
+    expect(el.children[0].tagName).toBe('BUTTON');
+    expect(el.children[0].innerText).toBe('Sign in');
+  });
+});
