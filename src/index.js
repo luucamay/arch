3349b4b30d61b5358f030b890e90c0c396c5608b
Imilla Dev <lupemaydanda@gmail.com>
2020-08-07 19:56:15 -0400

    feat: Add props and High Order Components that has a state

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import render from './lib/render';
 import SignIn from './pages/SignIn';
 
-const withState = 
-  Component => 
-    props => Component({ ...props, state: {} })
+const withState =
+  Component =>
+    props => Component({ ...props, state: { loading: false } });
 
 render(withState(SignIn), document.getElementById('root'));
--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,12 +1,16 @@
-const SignInButton = () => {
+const SignInButton = (props) => {
   const btn = document.createElement('button');
   btn.textContent = 'Sign In';
+  if (props.state.loading) {
+    btn.disabled = 'true';
+  }
   return btn;
-}
+};
 
-export default () => {
+export default (props) => {
+  console.log(props);
   const el = document.createElement('div');
-  el.className = 'sign-in';
-  el.appendChild(SignInButton());
+  el.className = 'sign-in'; 
+  el.appendChild(SignInButton(props));
   return el;
 };
