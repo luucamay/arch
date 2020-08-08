f10616a00f358600a9ee635d753ab945b6cd573f
Imilla Dev <lupemaydanda@gmail.com>
2020-08-07 20:09:27 -0400

    feat: Add the store to the props into the signIn components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import render from './lib/render';
+import createStore from './lib/createStore';
 import SignIn from './pages/SignIn';
 
+const store = createStore({ loading: false });
 const withState =
   Component =>
-    props => Component({ ...props, state: { loading: false } });
+    props => Component({ ...props, store });
 
 render(withState(SignIn), document.getElementById('root'));
--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,14 +1,14 @@
 const SignInButton = (props) => {
   const btn = document.createElement('button');
   btn.textContent = 'Sign In';
-  if (props.state.loading) {
+  if (props.store.getState().loading) {
     btn.disabled = 'true';
   }
   return btn;
 };
 
 export default (props) => {
-  console.log(props);
+  console.log(props.store.getState().loading);
   const el = document.createElement('div');
   el.className = 'sign-in'; 
   el.appendChild(SignInButton(props));
