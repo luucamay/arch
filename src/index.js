de0cc9f0d13c2b787f0a0e502adfac02cbffad6d
Imilla Dev <lupemaydanda@gmail.com>
2020-08-08 12:54:45 -0400

    feat: Get user data when sign in with google provider

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import render from './lib/render';
 import store, { withStore } from './store';
-import { withAuth } from './lib/auth';
+// import { withAuth } from './lib/auth';
+import { withFirebase } from './lib/firebase';
 import App from './App';
 
 const doRender = () =>
-  render(withAuth(withStore(App)), document.getElementById('root'));
+  render(
+    withStore(withFirebase(App)),
+    document.getElementById('root')
+    );
 
 store.suscribe(doRender);
 
--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,13 +1,15 @@
-const SignInButton = ({ store, auth }) => {
+const SignInButton = ({ store, firebase }) => {
   const btn = document.createElement('button');
   btn.textContent = 'Sign In';
   btn.onclick = () => {
     store.setState({ loading: true });
-    auth.signIn()
-      .then(user => store.setState({
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider)
+      .then(({ user }) => store.setState({
         loading: false,
         user,
-      }));
+      }))
+      .catch( erro => console.log);
   };
 
   if (store.getState().loading) {
@@ -17,7 +19,7 @@ const SignInButton = ({ store, auth }) => {
 };
 
 export default (props) => {
-  // console.log(props);
+  console.log(props);
   const el = document.createElement('div');
   el.className = 'sign-in';
   el.appendChild(SignInButton(props));
